fe5d15f52daf0542b28118ebb7a359e6523e22ef
c608393 <c608393@138.232.114.200>
2016-08-29 11:02:35 +0200

    new fulltextsearchresult object

---
+++ src/main/java/eu/transkribus/core/model/beans/searchresult/FulltextSearchResult.java
@@ -0,0 +1,49 @@
+package eu.transkribus.core.model.beans.searchresult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class FulltextSearchResult {
+	
+	protected String params;	
+	protected long numResults;
+	@XmlElementWrapper(name="pageHits")
+	@XmlElement(name="PageHit")
+	protected List<PageHit> pageHits = new ArrayList<>();
+	
+	public FulltextSearchResult(){}
+	
+	public List<PageHit> getPageHits() {
+		return pageHits;
+	}
+	
+	public void setPageHits(List<PageHit> pageHits) {
+		this.pageHits = pageHits;
+	}
+	
+	public long getNumResults() {
+		return numResults;
+	}
+	
+	public void setNumResults(long numResults) {
+		this.numResults = numResults;
+	}	
+	
+	public String getParams() {
+		return params;
+	}
+	
+	public void setParams(String params) {
+		this.params = params;
+	}
+
+
+}
---
+++ src/main/java/eu/transkribus/core/model/beans/searchresult/PageHit.java
@@ -0,0 +1,63 @@
+package eu.transkribus.core.model.beans.searchresult;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import eu.transkribus.core.model.beans.pagecontent_trp.TrpWordType;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PageHit {
+	private long docId;
+	private long pageNr;
+	private String pageUrl;	
+	private ArrayList<String> highlights = new ArrayList<>();
+	private ArrayList<String> wordCoords = new ArrayList<>();
+	
+	
+	public PageHit(){}
+	
+	public ArrayList<String> getHighlights() {
+		return highlights;
+	}
+
+	public void setHighlights(ArrayList<String> highlights) {
+		this.highlights = highlights;
+	}
+
+	public long getPageNr() {
+		return pageNr;
+	}
+
+	public void setPageNr(long pageNr) {
+		this.pageNr = pageNr;
+	}
+
+	public String getPageUrl() {
+		return pageUrl;
+	}
+
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+
+	public long getDocId() {
+		return docId;
+	}
+
+	public void setDocId(long docId) {
+		this.docId = docId;
+	}
+
+	public ArrayList<String> getWordCoords() {
+		return wordCoords;
+	}
+
+	public void setWordCoords(ArrayList<String> wordCoords) {
+		this.wordCoords = wordCoords;
+	}
+
+}
