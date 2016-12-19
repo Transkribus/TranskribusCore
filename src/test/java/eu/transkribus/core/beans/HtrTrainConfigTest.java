e4a85909b4cbe53520275150f86972281d2ed772
kahlep <philip.kahle@uibk.ac.at>
2016-12-19 15:10:44 +0100

    rename UroHtrTrainConfig

--- src/test/java/eu/transkribus/core/beans/HtrTrainConfigTest.java
+++ src/test/java/eu/transkribus/core/beans/HtrTrainConfigTest.java
@@ -5,12 +5,21 @@ import javax.xml.bind.JAXBException;
 import eu.transkribus.core.model.beans.DocumentSelectionDescriptor;
 import eu.transkribus.core.model.beans.DocumentSelectionDescriptor.PageDescriptor;
 import eu.transkribus.core.model.beans.HtrTrainConfig;
-import eu.transkribus.core.model.beans.UroHtrTrainConfig;
+import eu.transkribus.core.model.beans.CitLabHtrTrainConfig;
 import eu.transkribus.core.util.JaxbUtils;
 
 public class HtrTrainConfigTest {
 	public static void main(String[] args) throws JAXBException{
-		HtrTrainConfig htc = new UroHtrTrainConfig();
+		CitLabHtrTrainConfig htc = new CitLabHtrTrainConfig();
+		
+		htc.setColId(2);
+		htc.setDescription("A description");
+		htc.setLanguage("German");
+		htc.setLearningRate("2e-3");
+		htc.setNoise("both");
+		htc.setNumEpochs(200);
+		htc.setTrainSizePerEpoch(1000);
+		htc.setModelName("Test Model");
 		
 		for(int i = 1; i < 3; i++) {
 			DocumentSelectionDescriptor d = new DocumentSelectionDescriptor();
