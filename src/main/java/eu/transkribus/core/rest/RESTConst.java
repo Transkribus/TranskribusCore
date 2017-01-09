6e821e583de9c0a9f44d76f1ee38e0d0db70a8a8
maulwurf <maulwurf@maulwurf.maulwurf>
2017-01-09 15:33:28 +0100

    parameter for client retrieval for mac

--- src/main/java/eu/transkribus/core/rest/RESTConst.java
+++ src/main/java/eu/transkribus/core/rest/RESTConst.java
@@ -132,6 +132,8 @@ public final class RESTConst {
 	public static final String LOG_FILE_PARAM = "logFile";
 	
 	public static final String IS_RELEASE_PARAM = "isRelease";
+	public static final String PACKAGE_TYPE_PARAM = "packageType";
+	
 	public static final String FILE_NAME_PARAM = "fileName";
 	public static final String LIBS_MAP_PARAM = "libsMap";
 	
