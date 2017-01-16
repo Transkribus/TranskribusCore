51c6cee9c83831a6ce1b07ebc56b05f4853c75f6
maulwurf <maulwurf@maulwurf.maulwurf>
2017-01-16 14:14:12 +0100

    timestamp adapter v2

--- src/main/java/eu/transkribus/core/model/beans/adapters/SqlTimestampAdapter.java
+++ src/main/java/eu/transkribus/core/model/beans/adapters/SqlTimestampAdapter.java
@@ -5,19 +5,12 @@ import java.util.Date;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class SqlTimestampAdapter extends XmlAdapter<java.util.Date, java.sql.Timestamp> {
-	private static final Logger logger = LoggerFactory.getLogger(SqlTimestampAdapter.class);
-
 	@Override
 	public Timestamp unmarshal(Date v) throws Exception {
 		if (v == null)
 			return null;
-		
-		logger.debug("unmarshalling timestamp: "+v);
-		
+
 		return new Timestamp(v.getTime());
 	}
 
@@ -25,8 +18,6 @@ public class SqlTimestampAdapter extends XmlAdapter<java.util.Date, java.sql.Tim
 	public Date marshal(Timestamp v) throws Exception {
 		if (v == null)
 			return null;
-		
-		logger.debug("marshalling timestamp: "+v);
 		
 		return v;
 	}
