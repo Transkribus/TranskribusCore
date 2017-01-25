0d545aca6cc8d8408efddb12bf37e38423165909
maulwurf <maulwurf@maulwurf.maulwurf>
2017-01-25 18:23:27 +0100

    AConf

--- src/main/java/eu/transkribus/core/io/util/AConf.java
+++ src/main/java/eu/transkribus/core/io/util/AConf.java
@@ -1,31 +1,33 @@
 package eu.transkribus.core.io.util;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eu.transkribus.core.util.CoreUtils;
+
 /**
  * Abstract utility class containing static methods for reading property files and mapping values to various data types
- * @author philip
- *
  */
 public abstract class AConf {
 		private static final Logger logger = LoggerFactory.getLogger(AConf.class);
-		protected static Properties props = new Properties();
 		
-//		protected AConf(String fn) {
-//			props = 
-//		}
-
-		public static String getString(String name){
+		protected String filename;
+		protected Properties props;
+		
+		protected AConf(String filename) throws IOException {
+			this.filename = filename;
+			loadProps();
+		}
+		
+		public String getString(String name){
 			return props.getProperty(name);
 		}
 		
-		public static Integer getInt(String name){
+		public Integer getInt(String name){
 			final String prop = props.getProperty(name);
 			Integer value = null;
 			if(prop != null){
@@ -38,11 +40,11 @@ public abstract class AConf {
 			return value;
 		}
 		
-		public static Pattern getPattern(String name){
+		public Pattern getPattern(String name){
 			return Pattern.compile(props.getProperty(name));
 		}
 		
-		public static boolean getBool(String name){
+		public boolean getBool(String name){
 			final String value = props.getProperty(name);
 			boolean bool = false;
 			if(value.equals("1") || value.equalsIgnoreCase("true")){
@@ -51,26 +53,19 @@ public abstract class AConf {
 			return bool;
 		}
 
-		protected static Properties loadProps(String filename){
-			logger.debug("Load properties file: " + filename);
-//			if(props == null){
-			props = new Properties();
-//			}
-			InputStream is = null;
-			try{
-				is = AConf.class.getClassLoader().getResourceAsStream(filename);
-				props.load(is);
-			} catch (Exception e) {
-				logger.debug("Could not find properties file: " + filename);
-			}
-			finally{
-				try {
-					is.close();
-				} catch (IOException e) {
-					logger.warn("Could not close resource stream");
-					//ignore
-				}
-			}
+		protected Properties loadProps() throws IOException {
+			props = CoreUtils.loadProperties(filename);
 			return props;
 		}
+		
+//		public void parseAndSetString(String propertyName) throws IOException, IllegalAccessException, InvocationTargetException {
+//			String value = props.getProperty(propertyName);
+//			
+//			logger.info(propertyName+" = "+value);
+//			
+//			if (StringUtils.isEmpty(value))
+//				throw new IOException(propertyName+" must be provided!");
+//			
+//			BeanUtils.setProperty(this, propertyName, value);
+//		}
 }
