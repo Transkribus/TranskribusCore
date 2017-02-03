229d025e405c67913acbbf11758f1c1262238f51
maulwurf <maulwurf@maulwurf.maulwurf>
2017-02-03 16:01:14 +0100

    JobImpl

--- src/main/java/eu/transkribus/core/model/beans/job/enums/JobImpl.java
+++ src/main/java/eu/transkribus/core/model/beans/job/enums/JobImpl.java
@@ -1,5 +1,8 @@
 package eu.transkribus.core.model.beans.job.enums;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum JobImpl {
 	DeleteDocumentJob(JobTask.DeleteDocument, JobTask.DeleteDocument.getLabel(), "DeleteDocJob", null),
 	MetsImportJob(JobTask.CreateDocument, JobTask.CreateDocument.getLabel(), "MetsImportJob", null),
@@ -23,19 +26,30 @@ public enum JobImpl {
 	
 	HmmHtrJob(JobTask.UpvlcHtr, "PRHLT " + JobTask.Htr.getLabel(), "HmmHtrJob", null),
 	HmmHtrTrainingJob(JobTask.UpvlcHtrTraining, "PRHLT " + JobTask.HtrTraining.getLabel(), "HmmHtrTrainingJob", null),
+	
 	RnnHtrJob(JobTask.Htr, "CITlab " + JobTask.Htr.getLabel(), "RnnHtrJob", null),
 	RnnHtrTrainingJob(JobTask.HtrTraining, "CITlab " + JobTask.HtrTraining.getLabel(), "RnnHtrTrainingJob", null),
+	
 	CITlabHtrTrainingJob(JobTask.HtrTraining, "CITlab " + JobTask.HtrTraining.getLabel(), "CITlabHtrTrainingJob", null),
+//	CITlabHtrTrainingJob2(JobTask.HtrTraining, "CITlab " + JobTask.HtrTraining.getLabel(), "CITlabHtrTrainingJob", null), // duplicate for new module!
+	
 	CITlabHtrJob(JobTask.Htr, "CITlab " + JobTask.Htr.getLabel(), "CITlabHtrJob", null),
+//	CITlabHtrJob2(JobTask.Htr, "CITlab " + JobTask.Htr.getLabel(), "CITlabHtrJob", null), // duplicate for new module!
+	
 	FinereaderOcrJob(JobTask.Ocr, JobTask.Ocr.getLabel(), "OcrJob", null),
+//	FinereaderOcrJob2(JobTask.Ocr, JobTask.Ocr.getLabel(), "OcrJob", null), // duplicate for new module!
 	
 	//for testing
-	DummyJob(JobTask.CreateDocument, "Dummy Job", "DummyJob", null);
+	DummyJob(JobTask.CreateDocument, "Dummy Job", "DummyJob", null),
+	DummyJob2(JobTask.CreateDocument, "Dummy Job 2", "DummyJob2", null);
+	
+	private final static Logger logger = LoggerFactory.getLogger(JobImpl.class);
 	
 	private JobTask task;
 	private String label;
 	private String className;
 	private String libName;
+	
 	JobImpl(JobTask task, String label, String className, String libName){
 		this.task = task;
 		this.label = label;
@@ -54,5 +68,19 @@ public enum JobImpl {
 	}
 	public String getLibName() {
 		return libName;
+	}
+	
+	public static JobImpl fromStr(String str) {
+		try {
+			return JobImpl.valueOf(str); 
+		}
+		catch (Exception e) {
+			logger.warn("Could not parse JobImpl from string: "+str+" - returning null value!");
+			return null;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(JobImpl.valueOf(null)); 
 	}
 }
