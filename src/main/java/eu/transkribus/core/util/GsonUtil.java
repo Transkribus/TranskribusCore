8e39c9e1803762575fb5027904c3c27e3d703c93
jkloe <jkloe@whatsoever.com>
2017-02-27 10:10:13 +0100

    TrpProperties & GsonUtil updates

--- src/main/java/eu/transkribus/core/util/GsonUtil.java
+++ src/main/java/eu/transkribus/core/util/GsonUtil.java
@@ -2,7 +2,9 @@ package eu.transkribus.core.util;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -13,8 +15,8 @@ public class GsonUtil {
 	
 	public static final Type STRING_LIST_TYPE = new TypeToken<List<String>>(){}.getType();
 	
-	
-	
+	public static final Type MAP_TYPE = new TypeToken<Map<String, Object>>(){}.getType();
+		
 	public static String toJson(Object o) {	
 		return GSON.toJson(o);
 	}
@@ -33,6 +35,18 @@ public class GsonUtil {
 			return new ArrayList<>();
 		}
 	}
+	
+	public static Map<String, Object> toMap(String json) {
+		return GSON.fromJson(json, MAP_TYPE);
+	}
+	
+	public static Map<String, Object> toMap2(String json) {
+		try {
+			return GSON.fromJson(json, MAP_TYPE);
+		} catch (Exception e) {
+			return new HashMap<>();
+		}
+	}	
 	
 	
 
