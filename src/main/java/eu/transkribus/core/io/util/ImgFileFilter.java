ed533bfbe4049b81a9b5f0a35b0411f735dc1433
kahlep <philip.kahle@uibk.ac.at>
2017-08-24 10:21:01 +0200

    add ImgFilenameFilter

--- src/main/java/eu/transkribus/core/io/util/ImgFileFilter.java
+++ src/main/java/eu/transkribus/core/io/util/ImgFileFilter.java
@@ -13,6 +13,7 @@ public class ImgFileFilter implements FileFilter {
 	public boolean accept(File pathname) {
 		final String mime = MimeTypes.getMimeType(FilenameUtils.getExtension(pathname.getName()));
 		//is allowed mimetype and not starts with ".", which may occur on mac
+		//FIXME pathname.isFile() does not have to be checked here?
 		return pathname.isFile() && !pathname.getName().startsWith(".") && ImgPriority.priorities.containsKey(mime);
 	}
 }
