256baef8c494214b0c953b647557b6c2df32d5fb
kahlep <philip.kahle@uibk.ac.at>
2017-11-22 15:46:46 +0100

    Heldenbuch PAGE XML 600dpi to 300dpi translation

--- src/main/java/eu/transkribus/core/tools/ScalePageCoordinatesToImageDimension.java
+++ src/main/java/eu/transkribus/core/tools/ScalePageCoordinatesToImageDimension.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Files;
 
-import eu.transkribus.core.io.LocalDocReader;
 import eu.transkribus.core.io.util.ExtensionFileFilter;
 import eu.transkribus.core.model.beans.TrpDoc;
 import eu.transkribus.core.model.beans.TrpPage;
@@ -22,14 +21,16 @@ import eu.transkribus.core.util.PageXmlUtils;
 public class ScalePageCoordinatesToImageDimension {
 	private static final Logger logger = LoggerFactory.getLogger(ScalePageCoordinatesToImageDimension.class);
 	public static void main(String[] args) throws IOException, JAXBException {
+		
+		heldenbuch600to300dpi();
+		
+//		final String path = "/media/daten/Dokumente/newseye_testdata/";
 
-		final String path = "/media/daten/Dokumente/newseye_testdata/";
-
-		TrpDoc doc = LocalDocReader.load(path);
+//		TrpDoc doc = LocalDocReader.load(path);
 		
 //		fixAltoFilenames();
 
-		fixAltoMmToPx(doc);
+//		fixAltoMmToPx(doc);
 	}
 
 	private static void fixAltoFilenames() throws IOException {
@@ -54,6 +55,30 @@ public class ScalePageCoordinatesToImageDimension {
 			System.out.println("Processing file: " + f.getName());
 			PcGtsType pc = PageXmlUtils.unmarshal(f);
 			TrpPageTypeUtils.applyAffineTransformation(pc.getPage(), 0, 0, 2, 2, 0);
+
+			final String filename;
+			if (f.getName().contains("_")) {
+				filename = f.getName().split("_")[1];
+			} else {
+				filename = f.getName();
+			}
+			System.out.println("Writing file: " + filename);
+			PageXmlUtils.marshalToFile(pc, new File(output.getAbsolutePath() + File.separator + filename));
+		}
+	}
+	
+	private static void heldenbuch600to300dpi() throws JAXBException, IOException {
+		File input = new File("/tmp/Heldenbuch_600dpi/Heldenbuch_600dpi/page");
+		File output = new File("/tmp/Heldenbuch_600dpi/page_edited");
+		if (!output.isDirectory()) {
+			output.mkdirs();
+		}
+
+		File[] files = input.listFiles(new ExtensionFileFilter("xml", true, false));
+		for (File f : files) {
+			System.out.println("Processing file: " + f.getName());
+			PcGtsType pc = PageXmlUtils.unmarshal(f);
+			TrpPageTypeUtils.applyAffineTransformation(pc.getPage(), 0, 0, 0.5, 0.5, 0);
 
 			final String filename;
 			if (f.getName().contains("_")) {
