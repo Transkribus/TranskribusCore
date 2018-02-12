6521ecc2594c395a72b4a2531590589fc3563b91
fd17 <felix.dietrich@student.uibk.ac.at>
2018-02-12 17:05:43 +0100

    keyword solr search tools

---
+++ src/main/java/eu/transkribus/core/model/beans/searchresult/KeywordHit.java
@@ -0,0 +1,73 @@
+package eu.transkribus.core.model.beans.searchresult;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class KeywordHit {
+	
+	private String id;
+	private String docTitle;
+	private int pageNr;
+	private ArrayList<Integer> colIds;
+	private String pageUrl;
+	private String textCoords;
+	private String word;
+	private float probability;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDocTitle() {
+		return docTitle;
+	}
+	public void setDocTitle(String docTitle) {
+		this.docTitle = docTitle;
+	}
+	public ArrayList<Integer> getColIds() {
+		return colIds;
+	}
+	public void setColIds(ArrayList<Integer> colIds) {
+		this.colIds = colIds;
+	}
+	public String getPageUrl() {
+		return pageUrl;
+	}
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+	public int getPageNr() {
+		return pageNr;
+	}
+	public void setPageNr(int pageNr) {
+		this.pageNr = pageNr;
+	}
+	public String getTextCoords() {
+		return textCoords;
+	}
+	public void setTextCoords(String textCoords) {
+		this.textCoords = textCoords;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public float getProbability() {
+		return probability;
+	}
+	public void setProbability(float probability) {
+		this.probability = probability;
+	}
+	
+	
+
+}
---
+++ src/main/java/eu/transkribus/core/model/beans/searchresult/KeywordSearchResult.java
@@ -0,0 +1,53 @@
+package eu.transkribus.core.model.beans.searchresult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class KeywordSearchResult {
+	
+	private String params;
+	private long numResults;
+	private List<KeywordHit> keywordHits = new ArrayList<>();
+	
+	
+	public KeywordSearchResult(){}
+
+
+	public long getNumResults() {
+		return numResults;
+	}
+
+
+	public void setNumResults(long numResults) {
+		this.numResults = numResults;
+	}
+
+
+	public List<KeywordHit> getKeywordHits() {
+		return keywordHits;
+	}
+
+
+	public void setKeywordHits(List<KeywordHit> keywordHits) {
+		this.keywordHits = keywordHits;
+	}
+
+
+	public String getParams() {
+		return params;
+	}
+
+
+	public void setParams(String params) {
+		this.params = params;
+	}
+	
+	
+
+}
