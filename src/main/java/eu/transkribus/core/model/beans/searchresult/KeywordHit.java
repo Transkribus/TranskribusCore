e058b9c88c6f88b3386a1ae850725ad212954ea4
fd17 <felix.dietrich@student.uibk.ac.at>
2018-09-05 16:47:31 +0200

    new solrKWS

--- src/main/java/eu/transkribus/core/model/beans/searchresult/KeywordHit.java
+++ src/main/java/eu/transkribus/core/model/beans/searchresult/KeywordHit.java
@@ -10,49 +10,53 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class KeywordHit {
 	
-//	private String id;
-//	private String docTitle;
-//	private int pageNr;
-//	private ArrayList<Integer> colIds;
+	private String id;
+	private int docId;
+	private String docTitle;
+	private int pageNr;
+	private String imgKey;
+	private ArrayList<Integer> colIds;
 //	private ArrayList<String> wordOptions;
-//	private String pageUrl;
+	private String pageUrl;
 	private String lineId;
 	private String textCoords;
+	private ArrayList<Integer> pos;
+	private ArrayList<Integer> size;
 	private String word;
 	private float probability;
 	
-	private KeywordPageHit page;
+//	private KeywordPageHit page;
 	
-//	public String getId() {
-//		return id;
-//	}
-//	public void setId(String id) {
-//		this.id = id;
-//	}
-//	public String getDocTitle() {
-//		return docTitle;
-//	}
-//	public void setDocTitle(String docTitle) {
-//		this.docTitle = docTitle;
-//	}
-//	public ArrayList<Integer> getColIds() {
-//		return colIds;
-//	}
-//	public void setColIds(ArrayList<Integer> colIds) {
-//		this.colIds = colIds;
-//	}
-//	public String getPageUrl() {
-//		return pageUrl;
-//	}
-//	public void setPageUrl(String pageUrl) {
-//		this.pageUrl = pageUrl;
-//	}
-//	public int getPageNr() {
-//		return pageNr;
-//	}
-//	public void setPageNr(int pageNr) {
-//		this.pageNr = pageNr;
-//	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDocTitle() {
+		return docTitle;
+	}
+	public void setDocTitle(String docTitle) {
+		this.docTitle = docTitle;
+	}
+	public ArrayList<Integer> getColIds() {
+		return colIds;
+	}
+	public void setColIds(ArrayList<Integer> colIds) {
+		this.colIds = colIds;
+	}
+	public String getPageUrl() {
+		return pageUrl;
+	}
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+	public int getPageNr() {
+		return pageNr;
+	}
+	public void setPageNr(int pageNr) {
+		this.pageNr = pageNr;
+	}
 	public String getTextCoords() {
 		return textCoords;
 	}
@@ -83,11 +87,35 @@ public class KeywordHit {
 //	public void setWordOptions(ArrayList<String> wordOptions) {
 //		this.wordOptions = wordOptions;
 //	}
-	public KeywordPageHit getPage() {
-		return page;
+//	public KeywordPageHit getPage() {
+//		return page;
+//	}
+//	public void setPage(KeywordPageHit page) {
+//		this.page = page;
+//	}
+	public int getDocId() {
+		return docId;
 	}
-	public void setPage(KeywordPageHit page) {
-		this.page = page;
+	public void setDocId(int docId) {
+		this.docId = docId;
+	}
+	public String getImgKey() {
+		return imgKey;
+	}
+	public void setImgKey(String imgKey) {
+		this.imgKey = imgKey;
+	}
+	public ArrayList<Integer> getPos() {
+		return pos;
+	}
+	public void setPos(ArrayList<Integer> pos) {
+		this.pos = pos;
+	}
+	public ArrayList<Integer> getSize() {
+		return size;
+	}
+	public void setSize(ArrayList<Integer> size) {
+		this.size = size;
 	}
 	
 	
