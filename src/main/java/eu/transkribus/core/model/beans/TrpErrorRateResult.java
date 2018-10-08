69c90019da3451da79a567199883246840edcfab
kahlep <philip.kahle@uibk.ac.at>
2018-10-08 14:22:18 +0200

    TrpErrorRateResult: add Xml annotations to fix marshalling

--- src/main/java/eu/transkribus/core/model/beans/TrpErrorRateResult.java
+++ src/main/java/eu/transkribus/core/model/beans/TrpErrorRateResult.java
@@ -1,5 +1,11 @@
 package eu.transkribus.core.model.beans;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class TrpErrorRateResult {
 
 	private String wer;
