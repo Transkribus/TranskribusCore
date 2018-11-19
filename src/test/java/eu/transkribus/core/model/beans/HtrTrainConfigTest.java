8bc88009496716ba89289979e97aa31c65e03844
kahlep <philip.kahle@uibk.ac.at>
2018-11-19 15:48:59 +0100

    HtrTrainConfig tests

--- src/test/java/eu/transkribus/core/model/beans/HtrTrainConfigTest.java
+++ src/test/java/eu/transkribus/core/model/beans/HtrTrainConfigTest.java
@@ -8,35 +8,19 @@ import java.util.Properties;
 
 import javax.xml.bind.JAXBException;
 
-import eu.transkribus.core.model.beans.CitLabHtrTrainConfig;
-import eu.transkribus.core.model.beans.DocumentSelectionDescriptor;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import eu.transkribus.core.model.beans.DocumentSelectionDescriptor.PageDescriptor;
+import eu.transkribus.core.util.HtrCITlabUtils;
 import eu.transkribus.core.util.JaxbUtils;
 
 public class HtrTrainConfigTest {
+	private static final Logger logger = LoggerFactory.getLogger(HtrTrainConfigTest.class);
+	
 	public static void main(String[] args) throws JAXBException{
-		CitLabHtrTrainConfig htc = new CitLabHtrTrainConfig();
-		
-		htc.setColId(2);
-		htc.setDescription("A description");
-		htc.setLanguage("German");
-		htc.setLearningRate("2e-3");
-		htc.setNoise("both");
-		htc.setNumEpochs(200);
-		htc.setTrainSizePerEpoch(1000);
-		htc.setModelName("Test Model");
-		
-		for(int i = 1; i < 3; i++) {
-			DocumentSelectionDescriptor d = new DocumentSelectionDescriptor();
-			d.setDocId(i);
-			for(int j = 1; j < 3; j++) {
-				PageDescriptor p = new PageDescriptor();
-				p.setPageId(j);
-				p.setTsId(j);
-				d.getPages().add(p);
-			}
-			htc.getTrain().add(d);
-		}
+		CitLabHtrTrainConfig htc = createConfig();
 		
 //		System.out.println(JaxbUtils.marshalToString(htc, DocumentSelectionDescriptor.class, PageDescriptor.class));
 		Properties props = new Properties();
@@ -61,5 +45,66 @@ public class HtrTrainConfigTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static CitLabHtrTrainConfig createConfig() {
+		CitLabHtrTrainConfig htc = new CitLabHtrTrainConfig();
+		
+		htc.setColId(2);
+		htc.setDescription("A description");
+		htc.setLanguage("German");
+		htc.setLearningRate("2e-3");
+		htc.setNoise("both");
+		htc.setNumEpochs(200);
+		htc.setTrainSizePerEpoch(1000);
+		htc.setModelName("Test Model");
+		
+		for(int i = 1; i < 3; i++) {
+			DocumentSelectionDescriptor d = new DocumentSelectionDescriptor();
+			d.setDocId(i);
+			for(int j = 1; j < 3; j++) {
+				PageDescriptor p = new PageDescriptor();
+				p.setPageId(j);
+				p.setTsId(j);
+				d.getPages().add(p);
+			}
+			htc.getTrain().add(d);
+		}
+		return htc;
+	}
+	
+	private static CitLabHtrTrainConfig createPlusConfig() {
+		CitLabHtrTrainConfig htc = new CitLabHtrTrainConfig();
+		
+		htc.setColId(2);
+		htc.setDescription("A description");
+		htc.setLanguage("German");
+		htc.setLearningRate("2e-3");
+		htc.setNoise("both");
+		htc.setNumEpochs(200);
+		htc.setTrainSizePerEpoch(1000);
+		htc.setProvider(HtrCITlabUtils.PROVIDER_CITLAB_PLUS);
+		htc.setModelName("Test Model");
+		
+		for(int i = 1; i < 3; i++) {
+			DocumentSelectionDescriptor d = new DocumentSelectionDescriptor();
+			d.setDocId(i);
+			for(int j = 1; j < 3; j++) {
+				PageDescriptor p = new PageDescriptor();
+				p.setPageId(j);
+				p.setTsId(j);
+				d.getPages().add(p);
+			}
+			htc.getTrain().add(d);
+		}
+		return htc;
+	}
+	
+	@Test
+	public void testConfig() throws JAXBException {
+		CitLabHtrTrainConfig htc = createPlusConfig();
+		
+		logger.info(JaxbUtils.marshalToString(htc, DocumentSelectionDescriptor.class, PageDescriptor.class));
+		logger.info(JaxbUtils.marshalToJsonString(htc, true));
 	}
 }
