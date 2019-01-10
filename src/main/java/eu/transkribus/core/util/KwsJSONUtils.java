9eb46e0a4d28791a0dde26fafddc0bb3346798e7
fd17 <felix.dietrich@student.uibk.ac.at>
2019-01-10 17:17:04 +0100

    more debugging info for solrkws

--- src/main/java/eu/transkribus/core/util/KwsJSONUtils.java
+++ src/main/java/eu/transkribus/core/util/KwsJSONUtils.java
@@ -1,13 +1,16 @@
 package eu.transkribus.core.util;
 
 import java.io.File;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import org.eclipse.persistence.jaxb.JAXBContextProperties;
 
+import eu.transkribus.core.model.beans.searchresult.KeywordSearchResult;
 import eu.transkribus.core.model.beans.searchresult.KwPage;
 public class KwsJSONUtils {
 	
@@ -26,6 +29,37 @@ public class KwsJSONUtils {
 		KwPage page = (KwPage) ums.unmarshal(file);
 		
 		return page;
+	}
+	
+	public static String searchResultToXML(KeywordSearchResult result) throws JAXBException{
+		
+		JAXBContext jc = JAXBContext.newInstance(KeywordSearchResult.class);
+		Marshaller m = jc.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		
+		StringWriter sw = new StringWriter();
+		m.marshal(result, sw);
+		String xmlString = sw.toString();
+		
+		return xmlString;
+		
+		
+	}
+	
+	public static String searchResultToJSON(KeywordSearchResult result) throws JAXBException{
+		System.setProperty("javax.xml.bind.context.factory", "org.eclipse.persistence.jaxb.JAXBContextFactory");
+		JAXBContext jc = JAXBContext.newInstance(KeywordSearchResult.class);
+		Marshaller m = jc.createMarshaller();
+		m.setProperty(JAXBContextProperties.MEDIA_TYPE, "application/json");
+		m.setProperty(JAXBContextProperties.JSON_INCLUDE_ROOT, true);
+		
+		StringWriter sw = new StringWriter();
+		m.marshal(result, sw);
+		String jsonString = sw.toString();
+		
+		return jsonString;
+		
+		
 	}
 
 
