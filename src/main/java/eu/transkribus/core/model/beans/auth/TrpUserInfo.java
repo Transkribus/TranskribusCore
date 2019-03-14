170bd79fd278c9f488566d7719c5e10bb555d46d
krf41037 <floriankrull1507@yahoo.de>
2019-03-14 16:34:53 +0100

    Added bean UserInfo to display stats for collection

---
+++ src/main/java/eu/transkribus/core/model/beans/auth/TrpUserInfo.java
@@ -0,0 +1,106 @@
+package eu.transkribus.core.model.beans.auth;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import eu.transkribus.core.model.beans.TrpUserCollection;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TrpUserInfo implements Serializable {
+	
+	private static final long serialVersionUID = 7847909738857522789L;
+	protected String userName;
+	protected int uploads;
+	protected String training; 
+	
+	protected int htr;
+	protected int ocr;
+	protected int la;
+
+	protected static BigDecimal hosting;
+	
+	protected TrpUserCollection userCollection = null; 
+	
+	public TrpUserInfo() {}
+	
+	public TrpUserInfo(TrpUserInfo trpUserInfo) {
+	    this.userName = trpUserInfo.userName;
+	    this.uploads = trpUserInfo.uploads;
+	    this.training = trpUserInfo.training;
+	    this.htr = trpUserInfo.htr;
+	    this.ocr = trpUserInfo.ocr;
+	    this.la = trpUserInfo.la;
+	   
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Integer getUploads() {
+		return uploads;
+	}
+
+	public void setUploads(int uploads) {
+		this.uploads = uploads;
+	}
+
+	public String getTraining() {
+		return training;
+	}
+
+	public void setTraining(String training) {
+		this.training = training;
+	}
+
+	public int getHtr() {
+		return htr;
+	}
+
+	public void setHtr(int htrRuns) {
+		this.htr = htrRuns;
+	}
+
+	public int getOcr() {
+		return ocr;
+	}
+
+	public void setOcr(int ocr) {
+		this.ocr = ocr;
+	}
+
+	public BigDecimal getHosting() {
+		return hosting;
+	}
+
+	public void setHosting(BigDecimal hosting) {
+		TrpUserInfo.hosting = hosting;
+	}
+	
+	public int getLa() {
+		return la;
+	}
+
+	public void setLa(int la) {
+		this.la = la;
+	}
+
+	public TrpUserCollection getUserCollection() {
+		return userCollection;
+	}
+
+	public void setUserCollection(TrpUserCollection userCollection) {
+		this.userCollection = userCollection;
+	}
+	
+
+}
