59ec22eae8959a4926f8f6fae2e7a9eb97d0fbff
krf41037 <floriankrull1507@yahoo.de>
2019-12-02 11:20:03 +0100

    Changed to package visibility

--- src/main/java/eu/transkribus/core/model/beans/customtags/CustomTag.java
+++ src/main/java/eu/transkribus/core/model/beans/customtags/CustomTag.java
@@ -83,7 +83,7 @@ public class CustomTag implements Comparable<CustomTag>, Serializable {
 		this.attributes = new HashMap<CustomTagAttribute, Object>();		
 	}
 
-	private CustomTag(String tagName, int offset, int length) throws InitializationFailedException {
+	CustomTag(String tagName, int offset, int length) throws InitializationFailedException {
 		this(tagName);
 		setOffset(offset);
 		setLength(length);
