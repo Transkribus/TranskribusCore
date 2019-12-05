046ea6ebf1db7e538be3bb70ebfbc05b8fa565ea
krf41037 <floriankrull1507@yahoo.de>
2019-12-05 09:32:24 +0100

    Fixed duplicates

--- src/main/java/eu/transkribus/core/model/beans/job/enums/JobImpl.java
+++ src/main/java/eu/transkribus/core/model/beans/job/enums/JobImpl.java
@@ -21,10 +21,9 @@ public enum JobImpl {
 	//plain old DuplicateDocJob that copies all pages of a document to a new one.
 	DuplicateDocumentJob(JobTask.DuplicateDocument, JobTask.DuplicateDocument.getLabel(), "DuplicateDocJob", null),
 	//CopyJob that accepts DocumentSelectionDescriptors or GroundTruthSelectionDesctiptors for more control over the new document's content than DuplicateDocumentJob allows
-	CopyJob(JobTask.DuplicateDocument, JobTask.DuplicateDocument.getLabel(), "CopyJob", null),	CreateSampleDocJob(JobTask.CreateSample, JobTask.CreateSample.getLabel(), "CreateSampleDocJob", null),
+	CopyJob(JobTask.DuplicateDocument, JobTask.DuplicateDocument.getLabel(), "CopyJob", null),
 	ComputeSampleJob(JobTask.Htr,"Sample Computation","ComputeSampleJob",null),
 	CreateSampleDocJob(JobTask.CreateSample, JobTask.CreateSample.getLabel(), "CreateSampleDocJob", null),
-	ComputeSampleJob(JobTask.Htr,"Sample Computation","ComputeSampleJob",null),
 	DeleteDocJob(JobTask.DeleteDocument, JobTask.DeleteDocument.getLabel(), "DeleteDocJob", null),
 	RebuildSolrIndexJob(JobTask.RebuildSolrIndex, JobTask.RebuildSolrIndex.getLabel(), "RebuildSolrIndexJob", null),	
 	IndexDocumentJob(JobTask.IndexDocument, JobTask.IndexDocument.getLabel(), "IndexDocumentJob", null),
