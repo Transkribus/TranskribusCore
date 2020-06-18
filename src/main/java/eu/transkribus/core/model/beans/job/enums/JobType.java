73e648eb73f8138df619424a8adab753626ce691
kahlep <kahlep@users.noreply.github.com>
2020-06-18 14:40:55 +0200

    normalize JobImpl enum field values for use as credit type value

--- src/main/java/eu/transkribus/core/model/beans/job/enums/JobType.java
+++ src/main/java/eu/transkribus/core/model/beans/job/enums/JobType.java
@@ -4,28 +4,6 @@ public enum JobType {
 	layoutAnalysis,
 	utility,
 	recognition,
-	upvlc,
+	training,
 	kws;
-	
-//	layoutAnalysis("la"),
-//	utility("util"),
-//	recognition("recog");
-//	private String taskStr;
-//
-//	private Task(String t){
-//		taskStr = t;
-//	}
-//	
-//	public String getString(){
-//		return taskStr;
-//	}
-//	
-//	public static Task resolve(String str){
-//		for(Task t : Task.values()){
-//			if(t.getString().equals(str)) {
-//				return t;
-//			}
-//		}
-//		return null;
-//	}
 }
