4d34b107ec54ff207084d3601d44c7b84a291e77
jkloe <jkloe@jkloe.com>
2020-08-13 16:39:36 +0200

    fix CSVUtils

--- src/main/java/eu/transkribus/core/util/CSVUtils.java
+++ src/main/java/eu/transkribus/core/util/CSVUtils.java
@@ -1,66 +1,23 @@
 package eu.transkribus.core.util;
 
+import static eu.transkribus.core.util.CoreUtils.join;
+
 import java.io.IOException;
 import java.io.Writer;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-/**
- * {@link https://mkyong.com/java/how-to-export-data-to-csv-file-java/}
- * 
- * @author mkyong
- */
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public class CSVUtils {
 	public static final char DEFAULT_SEPARATOR = ',';
 	
 	public static void writeLineWoEncoding(Writer w, String line) throws IOException {
 		w.append(line+"\n");
 	}
-
-	public static void writeLine(Writer w, List<String> values) throws IOException {
-		writeLine(w, values, DEFAULT_SEPARATOR, ' ');
+	
+	public static void writeLine(Writer w, String... vals) throws IOException {
+		String line = join(Arrays.asList(vals).stream().map(s -> StringEscapeUtils.escapeCsv(s)).collect(Collectors.toList()));
+		w.write(line+"\n");
 	}
-
-	public static void writeLine(Writer w, List<String> values, char separators) throws IOException {
-		writeLine(w, values, separators, ' ');
-	}
-
-	// https://tools.ietf.org/html/rfc4180
-	private static String followCVSformat(String value) {
-
-		String result = value;
-		if (result.contains("\"")) {
-			result = result.replace("\"", "\"\"");
-		}
-		return result;
-
-	}
-
-	public static void writeLine(Writer w, List<String> values, char separators, char customQuote) throws IOException {
-
-		boolean first = true;
-
-		// default customQuote is empty
-
-		if (separators == ' ') {
-			separators = DEFAULT_SEPARATOR;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (String value : values) {
-			if (!first) {
-				sb.append(separators);
-			}
-			if (customQuote == ' ') {
-				sb.append(followCVSformat(value));
-			} else {
-				sb.append(customQuote).append(followCVSformat(value)).append(customQuote);
-			}
-
-			first = false;
-		}
-		sb.append("\n");
-		w.append(sb.toString());
-
-	}
-
 }
